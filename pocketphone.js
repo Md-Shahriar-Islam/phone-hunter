53df7c3b700276044200ab62ac46609cffcd3f46
Md Shahriar Islam <mdshahriarislam23@gmail.com>
2022-03-01 08:24:33 +0600

    details part of the card has been added

--- pocketphone.js
+++ pocketphone.js
@@ -1,3 +1,4 @@
+// -------api calling for showcase-----------
 const fetching = () => {
     const searchText = document.getElementById('search-box').value;
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
@@ -6,11 +7,68 @@ const fetching = () => {
         .then(data => showPhone(data.data));
 
 }
+// ------------api calling for details-------------
+const fetch_details = (data) => {
+    const url = `https://openapi.programming-hero.com/api/phone/${data}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => phone_details(data.data));
+}
+const phone_details = (data) => {
+    // console.log(data);
+    //------card body-------
+    const cardShow = document.getElementById('card-show');
+    cardShow.innerHTML = ``;
+    const body = document.createElement('div');
+    body.classList.add('card-body');
+    const bodyContainingDiv = document.createElement('div');
+    bodyContainingDiv.classList.add('col-md-8')
+    const { brand, image, name, releaseDate } = data;
+    body.innerHTML = `<h3>Brand:${brand}</h3>
+    <h4>Name:${name}</h4>`;
+
+    for (const property in data.mainFeatures) {
+        const p = document.createElement('p');
+        p.innerText = (`${property}: ${data.mainFeatures[property]}`);
+        body.appendChild(p);
+    }
+    for (const property in data.others) {
+        const p = document.createElement('p');
+        p.innerText = (`${property}: ${data.others[property]}`);
+        body.appendChild(p);
+    }
+
+    if (releaseDate === undefined || releaseDate === "") {
+        const p = document.createElement('p');
+        p.innerText = "releaseDate:no release date"
+        body.appendChild(p);
+    }
+    else {
+        const p = document.createElement('p');
+        p.innerText = `releaseDate:${releaseDate}`
+        body.appendChild(p);
+    }
+    bodyContainingDiv.appendChild(body);
+    // ------card image-------
+    const imgContainingDiv = document.createElement('div');
+    imgContainingDiv.classList.add('col-md-4');
+    imgContainingDiv.innerHTML = `<img src="${image}" class="img-fluid rounded-start" alt="...">`
+    // -------total containing div-----------
+    const divTotal = document.createElement('div');
+    divTotal.classList.add('row,g-0');
+    divTotal.appendChild(imgContainingDiv);
+    divTotal.appendChild(bodyContainingDiv);
+    // ---------html upload-----------
+
+    cardShow.appendChild(divTotal);
+    // console.log(sensors);
+}
+
 const showPhone = (data) => {
     const newMobile = data.slice(0, 20);
+    console.log(newMobile[0]);
     const phoneShowCase = document.getElementById('showcase');
     phoneShowCase.textContent = "";
-    console.log(newMobile[0]);
     newMobile.forEach(mobile => {
         const div = document.createElement('div');
         div.classList.add('col')
@@ -18,10 +76,11 @@ const showPhone = (data) => {
             `<div class="card">
             <img src="${mobile.image}" class="card-img-top w-50 pt-3 mx-auto" alt="...">
             <div class="card-body">
-                <h3 class="card-title">${mobile.brand}</h3>
-                <h4 class="card-text">${mobile.phone_name}r.</h4>
+                <h3 class="card-title text-center">${mobile.brand}</h3>
+                <h4 class="card-text text-center">${mobile.phone_name}</h4>
+                <button id="search-button" onClick="fetch_details('${mobile.slug}' )">details</button>
             </div>
-        </div>`
+        </div>`;
         phoneShowCase.appendChild(div);
 
     });
